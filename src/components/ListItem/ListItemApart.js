ab1aa77309f7a874fb00ea1154344e4c219f6cf8
doeuni <ehdms1115@naver.com>
2024-06-25 16:52:51 +0100

    [fix] 필터, 채팅 수정

--- src/components/ListItem/ListItemApart.js
+++ src/components/ListItem/ListItemApart.js
@@ -3,91 +3,63 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-export default function ListItem() {
-  const [propertyData, setPropertyData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const token = localStorage.getItem("token");
-      const headers = {
-        Authorization: `Bearer ${token}`,
-      };
-      const response = await axios.get(
-        "http://3.35.10.79:8080/realEstate/property/list",
-        {
-          headers: headers,
-        }
-      );
-      setPropertyData(response.data.result.content);
-      console.log(response.data.result.content)
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+export default function ListItem({ item }) {
 
   return (
-    <div className="list">
-      {propertyData.map((item) => (
-        <Link
-          key={item.propertyId}
-          to={`/detailapart/${item.propertyId}`}
-          className="list-item"
-          style={{ position: "relative" }} 
+    <Link
+      key={item.propertyId}
+      to={`/detailapart/${item.propertyId}`}
+      className="list-item"
+      style={{ position: "relative" }}
+    >
+      <div className="list-image">
+        <img
+          src={item.imageUrls}
+          alt="Property"
+          style={{ width: "80px", height: "auto" }}
+        />
+      </div>
+      <div
+        className="list-details"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <div className="list-title">{item.address.streetAddress}</div>
+        <div
+          className="info"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            marginRight: "10px",
+          }}
         >
-          <div className="list-image">
-            <img
-              src={item.imageUrls}
-              alt="Property"
-              style={{ width: "80px", height: "auto" }}
-            />
-          </div>
-          <div
-            className="list-details"
-            style={{ display: "flex", flexDirection: "column" }}
+          <div className="list-price">{item.weeklyFee}</div>
+          <p style={{ margin: 0, marginLeft: "5px" }}> /1주 </p>
+        </div>
+        <div
+          className="list-deta"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <div className="list-location">{item.address.city}</div>
+          <button
+            style={{
+              position: "absolute",
+              right: "10px",
+              borderRadius: "10px",
+              backgroundColor: "#D99E73",
+              color: "white",
+              border: "none",
+              padding: "5px 10px",
+              cursor: "pointer",
+            }}
           >
-            <div className="list-title">{item.address.streetAddress}</div>
-            <div
-              className="info"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                marginRight: "10px",
-              }}
-            >
-              <div className="list-price">{item.weeklyFee}</div>
-              <p style={{ margin: 0, marginLeft: "5px" }}> /1주 </p>
-            </div>
-            <div
-              className="list-deta"
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <div className="list-location">{item.address.city}</div>
-              <button
-                style={{
-                  position: "absolute",
-                  right: "10px",
-                  borderRadius: "10px",
-                  backgroundColor: "#D99E73",
-                  color: "white",
-                  border: "none",
-                  padding: "5px 10px",
-                  cursor: "pointer",
-                }}
-              >
-                상세보기
-              </button>
-            </div>
-          </div>
-        </Link>
-      ))}
-    </div>
+            상세보기
+          </button>
+        </div>
+      </div>
+    </Link>
   );
 }
